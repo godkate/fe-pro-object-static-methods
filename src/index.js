9d0272fda5221713b925912709123e56ecdfb2a0
godkate <bogodul883@gmail.com>
2022-11-27 21:43:06 -0500

    added script

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@
  * @param {Object} object
  * @returns {string[]}
  */
-export const keys = (object) => {};
+export const keys = (object) => {
+  let arr = [];
+  for (let key in object) {
+    arr.push(key);
+  }
+  return arr;
+};
 
 /**
  * Принимает объект, возвращает массив значений объекта. Также без использования
@@ -12,14 +18,26 @@ export const keys = (object) => {};
  * @param {Object} object
  * @returns {any[]}
  */
-export const values = (object) => {};
+export const values = (object) => {
+  let arr = [];
+  for (let key in object) {
+    arr.push(object[key]);
+  }
+  return arr;
+};
 
 /**
  * Принимаем объект, возвращаем энтри. Все также без методов, используем цикл
  * @param {Object} object
  * @returns {[string, any][]}
  */
-export const entries = (object) => {};
+export const entries = (object) => {
+  let arr = [];
+  for (let key in object) {
+    arr.push([key, object[key]]);
+  }
+  return arr;
+};
 
 /**
  * Тут наоборот, мы должны сделать из энтри объект обратно) Можете погуглить
@@ -27,4 +45,6 @@ export const entries = (object) => {};
  * @param {[string, any][]} entries
  * @returns {Object}
  */
-export const fromEntries = (entries) => {};
+export const fromEntries = (entries) => {
+  return Object.fromEntries(entries);
+};
